1996663a0c19a74a5941568f13f6b8cbd89c5c75
Himanshu Batham <112793814+himanshubathamx2@users.noreply.github.com>
2026-08-16 00:16:55 +0530

    Add: valid-tree - submission-19

---
+++ Data Structures & Algorithms/valid-tree/submission-19.java
@@ -0,0 +1,69 @@
+class Pain{
+    int node;
+    int parent;
+    Pain(int node, int parent){
+        this.node = node;
+        this.parent = parent;
+    }
+}
+
+class Solution {
+    public boolean validTree(int V, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        } 
+
+        int[] vis = new int[V];
+        int c = 0;
+        for(int i=0; i<V; i++){
+            if(vis[i] == 0){
+                if(c==1)
+                    return false;
+                if(!bfs(i, adj, vis)){
+                    return false;
+                }
+                c++;  
+            }
+        }
+        return true;
+    }
+
+    public boolean bfs(int i, List<List<Integer>> adj, int[] vis){
+        Queue<Pain> q = new LinkedList<>();
+        q.offer(new Pain(i, -1));
+        vis[i] = 1;
+        while(!q.isEmpty()){
+            Pain popped = q.poll();
+            int node = popped.node;
+            int parent = popped.parent;
+            for(int adja : adj.get(node)){
+                if(vis[adja] == 0){
+                    q.offer(new Pain(adja, node));
+                    vis[adja] = 1;
+                }else if(vis[adja] == 1 && adja != parent){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
